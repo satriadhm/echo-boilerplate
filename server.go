fe260951c1ff2257b7ddbba912218f26f3705be7
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2023-03-03 23:09:54 +0700

    adding update and delete controller function

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/Satria2133/echo-CRUD/auth"
 	"github.com/Satria2133/echo-CRUD/config"
+	controller "github.com/Satria2133/echo-CRUD/controller"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -24,9 +25,12 @@ func main() {
 	// Restricted group
 	r := e.Group("/restricted")
 	r.Use(echojwt.WithConfig(config.AuthConfig()))
-	r.GET("", auth.Restricted)
 
 	//routing CRUD
-
+	e.POST("/todo", controller.NewTodoList)
+	e.GET("/todo/:id", controller.GetTodoList)
+	e.PUT("/todo/:id", controller.UpdateTodoList)
+	e.DELETE("/todo/:id", controller.DeleteTodoList)
+	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
