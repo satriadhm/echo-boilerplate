a1d5b115fc186ce6cc0d1f1b8339304177b5ca85
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2023-03-03 23:10:34 +0700

    adding update and delete function

---
+++ controller/todoController.go
@@ -0,0 +1,64 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/Satria2133/echo-CRUD/model"
+	"github.com/labstack/echo/v4"
+)
+
+var Lock = sync.Mutex{}
+
+// NewTodoList ...
+func NewTodoList(c echo.Context) error {
+	Lock.Lock()
+	defer Lock.Unlock()
+	todo := &model.Todo{
+		Id: model.Seq,
+	}
+	if err := c.Bind(todo); err != nil {
+		return err
+	}
+	model.Todos[todo.Id] = todo
+	model.Seq++
+	return c.JSON(http.StatusCreated, todo)
+}
+
+// GetTodoList ...
+func GetTodoList(c echo.Context) error {
+	Lock.Lock()
+	defer Lock.Unlock()
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, model.Todos[id])
+}
+
+// UpdateTodoList ...
+
+func UpdateTodoList(c echo.Context) error {
+	Lock.Lock()
+	defer Lock.Unlock()
+	todo := new(model.Todo)
+	if err := c.Bind(todo); err != nil {
+		return err
+	}
+	if _, ok := model.Todos[todo.Id]; !ok {
+		return echo.NewHTTPError(http.StatusNotFound, model.ErrtodoNotFound.Error())
+	}
+	model.Todos[todo.Id] = todo
+	return c.JSON(http.StatusOK, todo)
+}
+
+// DeleteTodoList ...
+
+func DeleteTodoList(c echo.Context) error {
+	Lock.Lock()
+	defer Lock.Unlock()
+	id, _ := strconv.Atoi(c.Param("id"))
+	if _, ok := model.Todos[id]; !ok {
+		return echo.NewHTTPError(http.StatusNotFound, model.ErrtodoNotFound.Error())
+	}
+	delete(model.Todos, id)
+	return c.NoContent(http.StatusNoContent)
+}
